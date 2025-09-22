2e2001eb632523147c0bd7cdde9af6a2a9b4c743
bonefishcave <brucepaynej231@kesbath.com>
2025-09-22 14:57:53 +0000

    added a subroutine that changes all numbers in an entered string to an x
    added a subroutine to reverse a string

---
+++ Manipulating strings/Program.cs
@@ -0,0 +1,45 @@
+﻿class Program()
+{
+    static void Main(string[] args)
+    {
+        encryption();
+        reverseString();
+    }
+    static void encryption()
+    {
+        Console.WriteLine("Enter a decimal number to be encrypted: ");
+        string number = Console.ReadLine();
+        string encryptedNum = "";
+
+        for (int x = 0; x < number.Length; x++)
+        {
+            if (number[x] >= '0' && number[x] <= '9')
+            {
+                encryptedNum = encryptedNum + "x";
+            }
+            else
+            {
+                encryptedNum = encryptedNum + number[x];
+            }
+        }
+        Console.WriteLine(encryptedNum);
+    }
+
+    static void reverseString()
+    {
+        Console.WriteLine("Enter string to be reversed: ");
+        string userInput = Console.ReadLine();
+        string reversedString = "";
+
+        for (int x = userInput.Length - 1; x >= 0; x--) // this works off length being one more than the last position
+        {
+            reversedString = reversedString + userInput[x];
+        }
+        Console.WriteLine(reversedString);
+    }
+
+    static void upperCase()
+    {
+
+    }
+}
